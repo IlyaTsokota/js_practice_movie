38f2cc68eefe42686ee21620b077c8be75cbb22d
Ilya <iluha3211@gmail.com>
2021-02-25 00:27:54 +0200

    remove ads and refresh movies list

--- js/script.js
+++ js/script.js
@@ -15,12 +15,45 @@
 'use strict';
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+	movies: [
+		"Логан",
+		"Лига справедливости",
+		"Ла-ла лэнд",
+		"Одержимость",
+		"Скотт Пилигрим против..."
+	]
 };
 
+const adsBlocks = document.querySelectorAll('.promo__adv > img'),
+	promoGenre = document.querySelector('.promo__genre'),
+	promoBg = document.querySelector('.promo__bg'),
+	movieList = document.querySelector('.promo__interactive-list');
+
+function deleteAds(ads) {
+	ads.forEach(item => item.remove());
+}
+
+function setGenre(genre) {
+	promoGenre.textContent = genre;
+}
+
+function setBackgroundImage(elem, pathImg) {
+	elem.style.backgroundImage = `url('${pathImg}')`;
+}
+
+function refreshMovies(arr) {
+	let movies = "";
+	arr.forEach((item, i) => movies +=
+		`<li class="promo__interactive-item">
+			${i + 1}. ${item}
+			<div class="delete"></div>
+	</li>`
+	);
+
+	movieList.insertAdjacentHTML('afterbegin', movies);
+}
+
+deleteAds(adsBlocks);
+setGenre("драма");
+setBackgroundImage(promoBg, '../img/bg.jpg');
+refreshMovies(movieDB.movies);
